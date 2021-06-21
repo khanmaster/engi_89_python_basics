6a0468b1088312b6532ce638544a20fb6ffaa02c
Abdul Haseeb Shahrukh Khan <ahskhan@spartaglobal.com>
2021-06-21 14:02:59 +0100

    data types and operators added

---
+++ data_types_&_operators.py
@@ -0,0 +1,18 @@
+# What are Data Types and Operators
+# Boolean, Numers and Strings
+# Operators: Arithematic Oprators
+
+# - + - * / %
+# - is used to subtract the 2 values
+# + is used to add 2 values
+# * multiply
+# / divide
+# % modulus give the remainder
+
+# Comparison Operators
+# >
+# <
+# ==
+# !=
+# >=
+<=
